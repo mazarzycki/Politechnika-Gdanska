5df50a4abb1f23394b7417523b899fc813419ec8
mazarzycki <ma.zarzycki@yahoo.com>
2020-11-29 18:29:32 +0100

    JavaScript modified

--- script.js
+++ script.js
@@ -3,30 +3,37 @@ document.getElementById("open-contact-modal").addEventListener('click', openModa
 function openModal() {
     document.getElementById("contact").style.display = "block";
     var span = document.getElementById("close-modal");
-    span.onclick = function () {
+
+    span.addEventListener('click', function () {
         document.getElementById("contact").style.display = "none";
-    }
-    window.onclick = function (event) {
+    });
+
+    window.addEventListener('click', function (event) {
         if (event.target == document.getElementById("contact")) {
             document.getElementById("contact").style.display = "none";
         }
-    }
+    });
+
+
 }
 
 const parent = document.querySelector(".parent");
 parent.addEventListener("click", function (e) {
     const child = e.target.matches(".child, .child *");
     if (child) {
+
         document.getElementById("contact").style.display = "block";
         var span = document.getElementById("close-modal");
-        span.onclick = function () {
+
+        span.addEventListener('click', function () {
             document.getElementById("contact").style.display = "none";
-        }
-        window.onclick = function (event) {
+        });
+
+        window.addEventListener('click', function (event) {
             if (event.target == document.getElementById("contact")) {
                 document.getElementById("contact").style.display = "none";
             }
-        }
+        });
     }
 });
 
